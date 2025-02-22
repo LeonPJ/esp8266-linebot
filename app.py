818f6c9381a03fa2a2035db70339bf76cc876f1d
LeonPJ <leon19941214@gmail.com>
2025-02-22 19:53:03 +0800

    return user ID

--- app.py
+++ app.py
@@ -59,11 +59,14 @@ def handle_message(event):
         # GPT_answer = GPT_response(msg)
         # print(GPT_answer)
         # line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer)) 
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
+        if "TOKEN" in msg:
+            user_id = event.source.user_id
+            line_bot_api.reply_message(event.reply_token, TextSendMessage('${user_id}'))
+        else:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
     except:
         print(traceback.format_exc())
-        line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
-        
+        # line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
 
 @handler.add(PostbackEvent)
 def handle_message(event):
